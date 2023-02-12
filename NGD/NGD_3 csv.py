4d645c3942f8856258b666a85b6d076443f44738
Ryota Ando <andoooo9.y.1127@gmail.com>
2023-02-12 12:53:35 +0900

    全コード修正完了
    - 修正未完だったメソッドについても対応が完了。
    - 前コミットで修正したメソッド名がキャメルケースだったため、スネークケースに変更。

--- NGD/NGD_3 csv.py
+++ NGD/NGD_3 csv.py
@@ -6,6 +6,7 @@ import math
 import sys
 import re
 from urllib.parse import urlparse
+import pandas as pd
 
 import bs4
 import requests  # urlを読み込むためrequestsをインポート
@@ -30,7 +31,10 @@ class NgdProcessor:
     # インデックスの設定値（任意の値）
     INDEX_VALUE = 25270000000000
 
-    def getResponseBodyText(self, query:str) -> str:
+    # 書き込みcsvのヘッダーとなる値
+    FIELD_NAMES = ["Word_1", "Word_2", "NGD_Value"]
+
+    def get_response_body_text(self, query:str) -> str:
         """
         引数のqueryを検索ワードに設定後、Google検索を行い、レスポンスボディの結果をテキストとして返却する。
 
@@ -52,16 +56,17 @@ class NgdProcessor:
         except requests.exceptions.RequestException as e:
             print(e)
 
-    def extractResultStats(self, response_text:str) -> str:
+    def extract_result_stats_by_word(self, word:str) -> str:
         """
-        レスポンスボディのテキストから「検索結果の件数」を抽出し、返却する
+        検索単語によって取得したレスポンスボディのテキストから「検索結果の件数」を抽出し、返却する
 
         Args:
-            response_text (str): 検索結果のレスポンスボディ内容（テキスト）
+            word (str): 検索単語
 
         Returns:
             str: 検索結果件数（例.39800000）
         """
+        response_text = self.get_response_body_text(word)
         soup = bs4.BeautifulSoup(response_text, self.PARSER_SETTING)
         try:
             result_stats = soup.find("div", {"id": "result-stats"}).text
@@ -76,8 +81,9 @@ class NgdProcessor:
         return result
 
     # NGDの値を返す
-    def calculateNgd(self, fx_value:int, fy_value:int, fxy_value:int) -> float:
-        """NGDの値を計算し、返却する
+    def calculate_ngd(self, fx_value:int, fy_value:int, fxy_value:int) -> float:
+        """
+        NGDの値を計算し、返却する
 
         Args:
             fx_value (int): fx値
@@ -106,34 +112,71 @@ class NgdProcessor:
 
         return result
 
-    # TODO:あとで直す
-    def csvAutoLoader(readCsvFile, resultCsvFile):
-        # 書き込み専用のCSVファイル
-        with open(resultCsvFile, "w", encoding="utf-8") as write_file:
-            # 書き込みにあたってのheader宣言
-            fieldnames = ["Word_1", "Word_2", "NGD_Value"]
-            writer = csv.DictWriter(write_file, fieldnames=fieldnames)
+    def read_csv_to_list(self, r_path:str) -> list:
+        """
+        CSVを読み込み、リスト化して返却する
+        例えばCSVの中身が
+            apple,banana
+            wakayama,osaka
+        だった場合、下記のようなリストが生成される
+            [
+                ['apple','banana'],
+                ['wakayama','osaka']
+            ]
+
+        Args:
+            r_path (str): 読み込むcsvファイルのパス
+
+        Returns:
+            list: 2次元リスト
+        """
+        df = pd.read_csv(r_path, header=None)
+        result = df.values.tolist()
+
+        return result
+
+    def execute(self, r_path:str, w_path:str):
+        """
+        <メインの実行処理>
+        ①検索に使用する単語データの読み込み
+        ②検索単語を用いて検索結果件数の取得
+        ③NGDの算出
+        ④結果の書き込み
+        ⑤検索単語の組み合わせ分、②〜④を繰り返す
+
+        Args:
+            r_path (str): 読み込み先のCSVのファイルパス
+            w_path (str): 書き込み先のCSVのファイルパス
+        """
+        word_list = self.read_csv_to_list(r_path)
+        with open(w_path, "w", encoding="utf-8") as w_file:
+            writer = csv.DictWriter(w_file, fieldnames=self.FIELD_NAMES)
             writer.writeheader()
-            count_row = 0
-            # 読み込み専用のCSVファイル
-            with open(readCsvFile, encoding="utf-8") as read_file:
-                for row in read_file:
-                    count_row += 1
-                    searchCounts = []
-                    words = row.rstrip().split(",")
-                    for word in words:
-                        searchCounts.append(getSearchCount(word))
-                    searchCounts.append(getSearchCount(words[0] + "+" + words[1]))
-                    ngd = str(ngdCalculator(searchCounts))
-                    print(str(count_row) + "行目完了")
-                    writer.writerow(
-                        {"Word_1": words[0], "Word_2": words[1], "NGD_Value": ngd}
-                    )
+            for wl in word_list:
+                # 検索する2単語
+                word_1, word_2 = wl[0], wl[1]
 
-# TODO:あとで直す
-# 第一引数に調べたいファイル. 第二引数に結果として出力する際のファイル名を入れてください.
-csvAutoLoader("test.csv", "test_result.csv")
+                # word_1、word_2、word_1+word_2の3種類それぞれで検索した場合の検索結果件数を取得
+                fx_value = self.extract_result_stats_by_word(word_1)
+                fy_value = self.extract_result_stats_by_word(word_2)
+                fxy_value = self.extract_result_stats_by_word(word_1 + "+" + word_2)
 
+                # NGDの算出
+                ngd_result = self.calculate_ngd(fx_value, fy_value, fxy_value)
 
-# getSearchCount()
-# print('NGD:',ngdCalculator(searchCountlist))
+                # 結果の書き込み
+                writer.writerow(
+                    {
+                        self.FIELD_NAMES[0]: word_1
+                        , self.FIELD_NAMES[1]: word_2
+                        , self.FIELD_NAMES[2]: str(ngd_result)
+                    }
+                )
+
+                
+if __name__ == '__main__':
+    ngd_processor = NgdProcessor()
+    ngd_processor.execute(
+        r_path="test.csv"           # 読み込み先のCSVのファイルパス
+        , w_path="test_result.csv"  # 書き込み先のCSVのファイルパス
+    )
