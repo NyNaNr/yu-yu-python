63e077c7e80b3384df208916ec9aa6677618af2c
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-04-07 16:56:08 +0900

    second より簡潔に

--- Collatz_problem.py
+++ Collatz_problem.py
@@ -2,6 +2,7 @@ import sys
 
 
 def collatz():
+    print()
     try:
         number = int(input('数字を入力してください：'))
         print(number)
@@ -9,17 +10,14 @@ def collatz():
         print("エラー：数字以外の文字を入力しないでください。")
         sys.exit()
 
-    while True:
-        if number == 1:
-            print('finish')
-            break
+    while (number > 1):
         if number % 2 == 0:
             number = int(number/2)
 
-            print(number)
         else:
             number = int(number * 3 + 1)
-            print(number)
+
+        print(' →', number, end='')
 
 
 collatz()
