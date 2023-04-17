ffd3ea6be08cff9c819b34117f2049c582b64d04
NyNaNr <arbiter-firefly.0w@icloud.com>
2023-04-17 21:54:09 +0900

    見やすく整えた

--- renameDate.py
+++ renameDate.py
@@ -1,8 +1,10 @@
 # renameDate.py 米国式日付(MM-DD-YYYY)を欧州式日付(DD-MM-YYYY)に変換
 
-import shutil, os, re
+import shutil
+import os
+import re
 
-#米国式日付のファイル名にマッチする正規表現を作る。
+# 米国式日付のファイル名にマッチする正規表現を作る。
 
 date_pattern_america = re.compile(r'''
 ^(.*?) # 日付の前の全テキスト
@@ -18,12 +20,12 @@ print(os.getcwd())
 # カレントディレクトリの全ファイルをループする。
 for america_filename in os.listdir('.'):
     mo = date_pattern_america.search(america_filename)
-    
+
     # 日付のないファイルはスキップ
     if mo == None:
         continue
 
-    #ファイル名を分解
+    # ファイル名を分解
 
     before_part = mo.group(1)
     month_part = mo.group(2)
@@ -32,9 +34,9 @@ for america_filename in os.listdir('.'):
     after_part = mo.group(5)
 
     # 欧州式の日付のファイル名を作る。
-    euro_filename = before_part + date_part + '-' + month_part + '-' + year_part + after_part
+    euro_filename = before_part + date_part + '-' + \
+        month_part + '-' + year_part + after_part
 
     # ファイル名を変更する。
     print('Renaming "{}" to "{}" ...'.format(america_filename, euro_filename))
     # shutil.move(america_filename, euro_filename) #テスト後にコメントアウト
-
